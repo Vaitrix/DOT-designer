808b7a187cce8fd35a3ffa4d31d63553d220e62b
Vaitrix <37367712+Vaitrix@users.noreply.github.com>
2021-01-30 18:05:40 +0200

    corrected

---
+++ DOT_data.m
@@ -0,0 +1,10 @@
+classdef DOT_data
+    %UNTITLED25 Summary of this class goes here
+    %   Detailed explanation goes here
+    properties (Constant)
+        index = DOT_depository;
+    end
+    methods
+    end
+end
+
