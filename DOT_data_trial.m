4f6cca18dbebd102213eb85a943b44379df9e356
Vishnu Manoj <vishmj@gmail.com>
2022-01-22 17:15:57 +0200

    Add files via upload

---
+++ DOT_data_trial.m
@@ -0,0 +1,9 @@
+ classdef DOT_data_trial
+    %UNTITLED25 Summary of this class goes here
+    %   Detailed explanation goes here
+    properties (Constant)
+        index1 = DOT_depository_trial;
+    end
+    methods
+    end
+end
---
+++ DOT_depository_trial.m
@@ -0,0 +1,5 @@
+classdef DOT_depository_trial < handle
+    properties 
+        index1_prime
+    end
+end
